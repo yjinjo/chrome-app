7b73fcfacf8145f30c6f63aee4152a452366de9e
Youngjin Jo <yjinjo@berkeley.edu>
2022-11-09 22:12:02 +0900

    #1. Arrays

--- app.js
+++ app.js
@@ -1,15 +1,25 @@
 // app.js
 
-const amIFat = true;
-console.log(amIFat);     // true
+// without arrays
+const mon = "mon";
+const tue = "tue";
+const wed = "wed";
+const thu = "thu";
+const fri = "fri";
+const sat = "sat";
+const sun = "sun";
 
+const daysOfWeek = mon + tue + wed + thu + fri + sat + sun
 
-// null은 말 그대로 없다는 것이다. 
-// null은 절대 자연적으로 발생하지 않는다. 우리가 variable 안에 어떤 것이 없다는 것을 확실히 하기 위해 쓰는 것이다.
-const amIEmpty = null;  
-console.log(amIEmpty);   // null
+// console.log(daysOfWeek);  // montuewedthufrisatsun
 
-// variable은 만들고 있지만 값을 주지 않으면 undefined가 뜬다.
-// 즉, 컴퓨터 메모리 안에는 존재하지만 값이 들어가있지 않은 것이다. 
-let something;  
-console.log(something);  // undefined
+// with arrays
+const daysOfWeekArray = ["mon", "tue", "wed", "thu", "fri", "sat", "sun"]
+console.log(daysOfWeekArray);
+
+// Get Item from Array
+console.log(daysOfWeekArray[4]);
+
+// Add one more day to the array
+daysOfWeekArray.push("new");
+console.log(daysOfWeekArray);
