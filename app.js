5a8b3f650558754dc3dcdadf1ebf85765b2611b2
Youngjin Jo <yjinjo@berkeley.edu>
2022-11-09 21:50:13 +0900

    #1. const and let

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 // app.js
 
 const a = 5;
-const b = "2";
-
-const myName = "nico";
+const b = 2;
+let myName = "nico";
 
 console.log(a + b);
 console.log(a * b);
 console.log(a / b);
 console.log("hello " + myName);
+
+myName = "nicolas";
+console.log("your new name is " + myName);
