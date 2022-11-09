552ce1bef7fb710263d4b0a3afa360bf4bc1b33e
Youngjin Jo <yjinjo@berkeley.edu>
2022-11-09 21:58:53 +0900

    #1. Booleans, null, undefined

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
 // app.js
 
-const a = 5;
-const b = 2;
-let myName = "nico";
+const amIFat = true;
+console.log(amIFat);     // true
 
-console.log(a + b);
-console.log(a * b);
-console.log(a / b);
-console.log("hello " + myName);
 
-myName = "nicolas";
-console.log("your new name is " + myName);
+// null은 말 그대로 없다는 것이다. 
+// null은 절대 자연적으로 발생하지 않는다. 우리가 variable 안에 어떤 것이 없다는 것을 확실히 하기 위해 쓰는 것이다.
+const amIEmpty = null;  
+console.log(amIEmpty);   // null
+
+// variable은 만들고 있지만 값을 주지 않으면 undefined가 뜬다.
+// 즉, 컴퓨터 메모리 안에는 존재하지만 값이 들어가있지 않은 것이다. 
+let something;  
+console.log(something);  // undefined
