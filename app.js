2b31156add92987367634cf0a6ee9f1665c08e70
Youngjin Jo <yjinjo@berkeley.edu>
2022-11-11 11:23:58 +0900

    #2. CSS in JavaScript Part 3 - toggle function

--- app.js
+++ app.js
@@ -4,12 +4,7 @@ const h1 = document.querySelector(".hello h1");
 
 
 function handleTitleClick() {
-  const clickedClass = "clicked";
-  if (h1.classList.contains(clickedClass)) {
-    h1.classList.remove(clickedClass);
-  } else {
-    h1.classList.add(clickedClass);
-  }
+  h1.classList.toggle("clicked");
 }
 
 h1.addEventListener("click", handleTitleClick);
