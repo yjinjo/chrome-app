e85b5882692ce56abd337fec29d73c6499cd384d
Youngjin Jo <yjinjo@berkeley.edu>
2022-11-11 10:53:59 +0900

    #2. CSS in JavaScript Part One

--- app.js
+++ app.js
@@ -4,38 +4,19 @@ const h1 = document.querySelector(".hello h1");
 
 
 function handleTitleClick() {
-  h1.style.color = "blue";
+  const currentColor = h1.style.color;
+  let newColor;
+  if (currentColor === "blue") {
+    newColor = "tomato";
+  } else {
+    newColor = "blue";
+  }
+  h1.style.color = newColor;
 }
+
 
 function handleMouseEnter() {
   h1.innerText = "Mouse is here!"
 }
 
-function handleMouseLeave() {
-  h1.innerText = "Mouse is gone!";
-}
-
-function handleWindowResize() {
-  document.body.style.backgroundColor = "tomato";
-}
-
-function handleWindowCopy() {
-  alert("copier!");
-}
-
-function handleWindowOffline() {
-  alert("SOS no WIFI")
-}
-
-function handleWindowOnline() {
-  alert("ALL GOOD!")
-}
-
-h1.onclick = handleTitleClick;
-h1.addEventListener("mouseenter", handleMouseEnter);
-h1.addEventListener("mouseleave", handleMouseLeave);
-
-window.addEventListener("resize", handleWindowResize);
-window.addEventListener("copy", handleWindowCopy);
-window.addEventListener("offline", handleWindowOffline);
-window.addEventListener("online", handleWindowOnline);
+h1.addEventListener("click", handleTitleClick);
