6ed0f1c6caa48acb6df16482b25859e684cbea87
Youngjin Jo <yjinjo@berkeley.edu>
2022-11-11 12:19:41 +0900

    #3. Form Submission

--- app.js
+++ app.js
@@ -4,8 +4,8 @@ const loginInput = document.querySelector("#login-form input");
 const loginButton = document.querySelector("#login-form button");
 
 function onLoginBtnClick() {
-  console.log(loginInput.value);
-  console.log("click!!!!");
+  const username = loginInput.value;
+  console.log(username);
 }
 
-loginButton.addEventListener("click", onLoginBtnClick);
+
