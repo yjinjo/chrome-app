3ab776519e06c490f46b8ae61d7e44b2263fdc97
Youngjin Jo <yjinjo@berkeley.edu>
2022-11-11 12:04:20 +0900

    #3. Input Values

--- app.js
+++ app.js
@@ -1,2 +1,11 @@
 // app.js
 
+const loginInput = document.querySelector("#login-form input");
+const loginButton = document.querySelector("#login-form button");
+
+function onLoginBtnClick() {
+  console.log(loginInput.value);
+  console.log("click!!!!");
+}
+
+loginButton.addEventListener("click", onLoginBtnClick);
