68a4af340db948026c59753dde339808589a3757
Youngjin Jo <yjinjo@berkeley.edu>
2022-11-11 10:43:56 +0900

    #2. Events

--- app.js
+++ app.js
@@ -1,4 +1,41 @@
 // app.js
 
-const title = document.querySelector(".hello h1:first-child");
-console.log(title);
+const h1 = document.querySelector(".hello h1");
+
+
+function handleTitleClick() {
+  h1.style.color = "blue";
+}
+
+function handleMouseEnter() {
+  h1.innerText = "Mouse is here!"
+}
+
+function handleMouseLeave() {
+  h1.innerText = "Mouse is gone!";
+}
+
+function handleWindowResize() {
+  document.body.style.backgroundColor = "tomato";
+}
+
+function handleWindowCopy() {
+  alert("copier!");
+}
+
+function handleWindowOffline() {
+  alert("SOS no WIFI")
+}
+
+function handleWindowOnline() {
+  alert("ALL GOOD!")
+}
+
+h1.onclick = handleTitleClick;
+h1.addEventListener("mouseenter", handleMouseEnter);
+h1.addEventListener("mouseleave", handleMouseLeave);
+
+window.addEventListener("resize", handleWindowResize);
+window.addEventListener("copy", handleWindowCopy);
+window.addEventListener("offline", handleWindowOffline);
+window.addEventListener("online", handleWindowOnline);
