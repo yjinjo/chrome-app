14e958323363e922b912638cca11ad138b81f9a5
Youngjin Jo <yjinjo@berkeley.edu>
2022-11-11 11:51:29 +0900

    #3. Initializing the application

--- app.js
+++ app.js
@@ -1,10 +1,2 @@
 // app.js
 
-const h1 = document.querySelector(".hello h1");
-
-
-function handleTitleClick() {
-  h1.classList.toggle("clicked");
-}
-
-h1.addEventListener("click", handleTitleClick);
