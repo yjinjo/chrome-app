80f0c756f7843e78dcdb122f7b49d387d7e369e0
Youngjin Jo <yjinjo@berkeley.edu>
2022-11-12 14:45:33 +0900

    #6. Saving ToDos

--- js/todo.js
+++ js/todo.js
@@ -2,6 +2,12 @@ const toDoForm = document.getElementById("todo-form");
 const toDoInput = document.querySelector("#todo-form input");
 const toDoList = document.getElementById("todo-list");
 
+const toDos = [];
+
+function saveToDos() {
+  localStorage.setItem("todos", JSON.stringify(toDos));
+}
+
 function deleteToDo(event) {
   const li = event.target.parentElement;
   li.remove();
@@ -26,7 +32,9 @@ function handleToDoSubmit(event) {
   event.preventDefault();
   const newTodo = toDoInput.value;
   toDoInput.value = "";  // 입력하고 나면 입력창을 비움. 
+  toDos.push(newTodo);
   paintToDo(newTodo);
+  saveToDos();
 }
 
 // form 태그가 submit 할 때 새로고침 event를 발생시키므로 이를 처리할 방법이 있어야 한다.
