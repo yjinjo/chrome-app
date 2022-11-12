2de8fc4bf63dbe38cf115712290302c3c429a327
Youngjin Jo <yjinjo@berkeley.edu>
2022-11-12 14:16:57 +0900

    #6. Adding ToDos

--- js/todo.js
+++ js/todo.js
@@ -2,10 +2,19 @@ const toDoForm = document.getElementById("todo-form");
 const toDoInput = document.querySelector("#todo-form input");
 const toDoList = document.getElementById("todo-list");
 
+function paintToDo(newTodo) {
+  const li = document.createElement("li");
+  const span = document.createElement("span");
+  li.appendChild(span);
+  span.innerText = newTodo;
+  toDoList.appendChild(li);
+}
+
 function handleToDoSubmit(event) {
   event.preventDefault();
   const newTodo = toDoInput.value;
   toDoInput.value = "";  // 입력하고 나면 입력창을 비움. 
+  paintToDo(newTodo);
 }
 
 // form 태그가 submit 할 때 새로고침 event를 발생시키므로 이를 처리할 방법이 있어야 한다.
