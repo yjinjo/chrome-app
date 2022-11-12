2c07212904c064eda3ec7f4a195b3d558f61e28a
Youngjin Jo <yjinjo@berkeley.edu>
2022-11-12 09:26:39 +0900

    #3. Events

--- app.js
+++ app.js
@@ -1,11 +1,21 @@
 // app.js
 
+const loginForm = document.querySelector("#login-form")
 const loginInput = document.querySelector("#login-form input");
-const loginButton = document.querySelector("#login-form button");
 
-function onLoginBtnClick() {
-  const username = loginInput.value;
-  console.log(username);
+const link = document.querySelector("a");
+
+function onLoginSubmit(event) {
+  event.preventDefault();
+  console.log(loginInput.value);
 }
 
+function handleLinkClick(event) {
+  event.preventDefault();
+  console.log(event);
+  alert("clicked!!");
+}
 
+loginForm.addEventListener("submit", onLoginSubmit);
+// JS는 누군가 링크를 클릭할 때 인자에 해당하는 함수를 실행할 것이다. 
+link.addEventListener("click", handleLinkClick);
