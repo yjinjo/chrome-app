3af841e17dd3e19721081a3944b542105d6968a8
Youngjin Jo <yjinjo@berkeley.edu>
2022-11-12 22:02:31 +0900

    #6. Loading ToDos Part Two

--- js/todo.js
+++ js/todo.js
@@ -3,7 +3,7 @@ const toDoInput = document.querySelector("#todo-form input");
 const toDoList = document.getElementById("todo-list");
 
 const TODOS_KEY = "todos";
-const toDos = [];
+let toDos = [];
 
 function saveToDos() {
   localStorage.setItem(TODOS_KEY, JSON.stringify(toDos));
@@ -47,5 +47,6 @@ const savedToDos = localStorage.getItem(TODOS_KEY);
 if (savedToDos) {
   // array 형태로 만든다. 
   const parsedToDos = JSON.parse(savedToDos);
-  parsedToDos.forEach((item) => console.log("this is the turn off ", item));
+  toDos = parsedToDos;
+  parsedToDos.forEach(paintToDo);
 }
