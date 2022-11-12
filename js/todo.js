02c2a5d919f2549b9d5be001b215962b244fba09
Youngjin Jo <yjinjo@berkeley.edu>
2022-11-12 15:07:04 +0900

    #6. Loading ToDos Part One

--- js/todo.js
+++ js/todo.js
@@ -2,10 +2,12 @@ const toDoForm = document.getElementById("todo-form");
 const toDoInput = document.querySelector("#todo-form input");
 const toDoList = document.getElementById("todo-list");
 
+const TODOS_KEY = "todos";
 const toDos = [];
 
 function saveToDos() {
-  localStorage.setItem("todos", JSON.stringify(toDos));
+  localStorage.setItem(TODOS_KEY, JSON.stringify(toDos));
+
 }
 
 function deleteToDo(event) {
@@ -39,3 +41,11 @@ function handleToDoSubmit(event) {
 
 // form 태그가 submit 할 때 새로고침 event를 발생시키므로 이를 처리할 방법이 있어야 한다.
 toDoForm.addEventListener("submit", handleToDoSubmit);
+
+const savedToDos = localStorage.getItem(TODOS_KEY);
+// 만약 saveToDos가 localStorage에 존재한다면,
+if (savedToDos) {
+  // array 형태로 만든다. 
+  const parsedToDos = JSON.parse(savedToDos);
+  parsedToDos.forEach((item) => console.log("this is the turn off ", item));
+}
